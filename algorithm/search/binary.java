0bc8c6aa494df955fda3e20650b3d1c0ef8c9eff
Carrot97 <lixuhuiustb@163.com>
2020-06-04 16:41:10 +0800

    modify binarySearch

--- algorithm/search/binary.java
+++ algorithm/search/binary.java
@@ -1,4 +1,5 @@
 package search;
+
 import sort.Bubble;
 import java.util.Arrays;
 
@@ -17,14 +18,43 @@ public class binary {
         return -1;
     }
 
+    public static int rank(int[] array, int data) {
+        int count = 1;
+        int idx = search(array, data);
+        if (idx != -1) {
+            for (int i = 0; i < idx; i++) {
+                if (array[i] != array[i + 1] && array[i + 1] != data)
+                    count++;
+            }
+            return count;
+        }
+        else return -1;
+
+    }
+
+    public static int count(int[] array, int data) {
+        int counter = 1;
+        int idx = search(array, data);
+        if (idx != -1) {
+            for (int i = idx - 1; array[i] == data; i--)
+                counter++;
+            for (int i = idx + 1; array[i] == data; i++)
+                counter++;
+            return counter;
+        }
+        else return -1;
+    }
+
     public static void main(String[] args) {
         int[] data = new int[20];
         for(int i=0; i<data.length; i++){
-            data[i] = (int) (Math.random() * 100);
+            data[i] = (int) (Math.random() * 10);
         }
-        data[0] = 233;
+//        data[0] = 233;
         int[] dataSorted = Bubble.sort(data);
         System.out.println(Arrays.toString(dataSorted));
-        System.out.println(search(dataSorted, 233));
+        System.out.println(search(dataSorted, 2));
+        System.out.println(rank(dataSorted,2));
+        System.out.println(count(dataSorted, 2));
     }
 }
