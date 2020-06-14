8e8c5f7a6a9442402df9a43c76e2197af8f301f5
Carrot97 <lixuhuiustb@163.com>
2020-06-14 10:24:45 +0800

    多态性修改

--- algorithm/sort/MergeFRAdvanced.java
+++ algorithm/sort/MergeFRAdvanced.java
@@ -2,7 +2,13 @@ package sort;
 
 import java.util.Arrays;
 
-public class MergeFRAdvanced extends MergeFromRoof{
+public class MergeFRAdvanced {
+    protected static int[] aux;
+
+    public static void sort(int[] data) {
+        aux = new int[data.length];
+        sort(data, 0, data.length-1);
+    }
 
     public static void sort(int[] data, int lo, int hi) {
         if (lo < hi) {
@@ -15,6 +21,19 @@ public class MergeFRAdvanced extends MergeFromRoof{
             // 测试数组是否已经有序
             if (data[mid] > data[mid+1])
             merge(data, lo, mid, hi);
+        }
+    }
+
+    public static void merge(int[] data, int lo, int mid, int hi) {
+        int i = lo, j = mid+1;
+        // 复制数组
+        for (int k=lo; k <= hi; k++) aux[k] = data[k];
+        // 排序
+        for (int k=lo; k <= hi; k++) {
+            if (i > mid) data[k] = aux[j++];
+            else if (j > hi) data[k] = aux[i++];
+            else if (aux[j] < aux[i]) data[k] = aux[j++];
+            else  data[k] = aux[i++];
         }
     }
 
